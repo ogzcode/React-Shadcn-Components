3a2832dd76ba5655629b0c26a7cf27d0967f8090
ogzCode <oguzguc44@gmail.com>
2024-07-03 17:45:00 +0300

    chore: Add configuration files and ignore unnecessary files

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import mdx from '@mdx-js/rollup'
+
+export default defineConfig({
+  plugins: [react(),
+    mdx({
+      remarkPlugins: [],
+      rehypePlugins: [],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
